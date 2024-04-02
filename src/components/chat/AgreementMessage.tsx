ab26202f7e89946c18e06b40e2e8cde1b39d8aec
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2024-04-02 14:51:04 +0700

    refactor: clean up

--- src/components/chat/AgreementMessage.tsx
+++ src/components/chat/AgreementMessage.tsx
@@ -1,83 +1,97 @@
 import AgreementData from "@/models/AgreementData";
-import { ChatMessage } from "@/models/Chat";
 import getOneAgreement from "@/services/agreement/getOneAgreement";
-import getUserAgreement from "@/services/agreement/getUserAgreement";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function AgreementMessage({ message }: { message: string }) {
-    const [data, setData] = useState<AgreementData>()
-    const [img, setImg] = useState<string>("/img/Boss.png")
-    useEffect(() => {
-        async function getProperty() {
-            try {
-                const data = await getOneAgreement(message);
-                setData(data)
-                console.log(data.property.property_images[0])
-                if (data.property.property_images[0]) {
-                    setImg(data.property.property_images[0])
-                }
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
+  const [data, setData] = useState<AgreementData>();
+  const [img, setImg] = useState<string>("/img/Boss.png");
+  useEffect(() => {
+    async function getProperty() {
+      try {
+        const data = await getOneAgreement(message);
+        setData(data);
+        console.log(data.property.property_images[0]);
+        if (data.property.property_images[0]) {
+          setImg(data.property.property_images[0]);
         }
-        getProperty();
-    }, [])
-    return (
-        <div className="flex flex-col break-word rounded-xl bg-ci-light-gray px-4 py-3 w-full gap-y-2">
-            {data?.status == "AWAITING_DEPOSIT" &&
-                <div>
-                    You have created an agreement with {data?.owner.owner_first_name} {data?.owner.owner_last_name}
-                </div>
-            }
-
-            {data?.status == "AWAITING_PAYMENT" &&
-                <div>
-                    You have created an agreement with {data?.owner.owner_first_name} {data?.owner.owner_last_name}
-                </div>
-            }
-
-            {data?.status == "RENTING" &&
-                <div>
-                    Your agreement with {data?.owner.owner_first_name} {data?.owner.owner_last_name} have been confirmed
-                </div>
-            }
-
-            {data?.status == "OVERDUE" &&
-                <div>
-                    Your agreement are overdue
-                </div>
-            }
-
-            {data?.status == "CANCELLED" &&
-                <div>
-                    You have cancelled an appointment with {data?.owner.owner_first_name} {data?.owner.owner_last_name}
-                </div>
-            }
-            <span className="w-full border-t border-black"></span>
-            <div className="flex flex-row gap-x-3 items-center">
-                <div className="relative flex aspect-square w-16 h-16 items-center justify-center overflow-hidden rounded-lg">
-                    <Image
-                        src={img}
-                        alt="ไอบอสสสส"
-                        draggable={false}
-                        fill={true}
-                        style={{ objectFit: "cover" }}
-
-                    />
-                </div>
-                <div className="w-3/5 flex flex-col justify-center">
-                    <div className="text-lg font-bold">{data?.property.property_name}</div>
-                    <div className="break-all">Price: {data?.total_payment}$</div>
-                    <div className="flex flex-row gap-x-1 items-center">Status:
-                        {data?.status == "AWAITING_DEPOSIT" && <div className="bg-ci-yellow px-2 rounded-lg">Awaiting Deposit</div>}
-                        {data?.status == "AWAITING_PAYMENT" && <div className="bg-ci-yellow px-2 rounded-lg">Awaiting Payment</div>}
-                        {data?.status == "RENTING" && <div className="bg-ci-green px-2 rounded-lg">Renting</div>}
-                        {data?.status == "OVERDUE" && <div className="bg-ci-red px-2 rounded-lg">Overdue</div>}
-                        {data?.status == "CANCELLED" && <div className="bg-ci-red px-2 rounded-lg">Cancelled</div>}
-                    </div>
-                </div>
-            </div>
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    getProperty();
+  }, []);
+  return (
+    <div className="flex w-full flex-col gap-y-2 break-words rounded-xl bg-ci-light-gray px-4 py-3">
+      {data?.status == "AWAITING_DEPOSIT" && (
+        <div>
+          You have created an agreement with {data?.owner.owner_first_name}{" "}
+          {data?.owner.owner_last_name}
         </div>
-    );
+      )}
+
+      {data?.status == "AWAITING_PAYMENT" && (
+        <div>
+          You have created an agreement with {data?.owner.owner_first_name}{" "}
+          {data?.owner.owner_last_name}
+        </div>
+      )}
+
+      {data?.status == "RENTING" && (
+        <div>
+          Your agreement with {data?.owner.owner_first_name}{" "}
+          {data?.owner.owner_last_name} have been confirmed
+        </div>
+      )}
+
+      {data?.status == "OVERDUE" && <div>Your agreement are overdue</div>}
+
+      {data?.status == "CANCELLED" && (
+        <div>
+          You have cancelled an appointment with {data?.owner.owner_first_name}{" "}
+          {data?.owner.owner_last_name}
+        </div>
+      )}
+      <span className="w-full border-t border-black"></span>
+      <div className="flex flex-row items-center gap-x-3">
+        <div className="relative flex aspect-square size-16 items-center justify-center overflow-hidden rounded-lg">
+          <Image
+            src={img}
+            alt="ไอบอสสสส"
+            draggable={false}
+            fill={true}
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+        <div className="flex w-3/5 flex-col justify-center">
+          <div className="text-lg font-bold">
+            {data?.property.property_name}
+          </div>
+          <div className="break-all">Price: {data?.total_payment}$</div>
+          <div className="flex flex-row items-center gap-x-1">
+            Status:
+            {data?.status == "AWAITING_DEPOSIT" && (
+              <div className="rounded-lg bg-ci-yellow px-2">
+                Awaiting Deposit
+              </div>
+            )}
+            {data?.status == "AWAITING_PAYMENT" && (
+              <div className="rounded-lg bg-ci-yellow px-2">
+                Awaiting Payment
+              </div>
+            )}
+            {data?.status == "RENTING" && (
+              <div className="rounded-lg bg-ci-green px-2">Renting</div>
+            )}
+            {data?.status == "OVERDUE" && (
+              <div className="rounded-lg bg-ci-red px-2">Overdue</div>
+            )}
+            {data?.status == "CANCELLED" && (
+              <div className="rounded-lg bg-ci-red px-2">Cancelled</div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/chat/PropertyMessage.tsx
+++ src/components/chat/PropertyMessage.tsx
@@ -1,52 +1,47 @@
-import { ChatMessage } from "@/models/Chat";
 import PropertyData from "@/models/PropertyData";
 import getPropertyDetail from "@/services/property/getPropertyDetail";
-import { StaticImport } from "next/dist/shared/lib/get-img-props";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function PropetyMessage({ message }: { message: string }) {
-    const [data, setData] = useState<PropertyData>()
-    const [img, setImg] = useState<string>("/img/Boss.png")
-    useEffect(() => {
-        async function getProperty() {
-            try {
-                const data = await getPropertyDetail(message);
-                setData(data)
-                if (data.property_images[0]) {
-                    setImg(data.property_images[0])
-                }
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
+  const [data, setData] = useState<PropertyData>();
+  const [img, setImg] = useState<string>("/img/Boss.png");
+  useEffect(() => {
+    async function getProperty() {
+      try {
+        const data = await getPropertyDetail(message);
+        setData(data);
+        if (data.property_images[0]) {
+          setImg(data.property_images[0]);
         }
-        getProperty();
-    }, [])
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    getProperty();
+  }, []);
 
-    return (
-        <div className="flex flex-col break-word rounded-xl bg-ci-light-gray px-4 py-3 w-full gap-y-2">
-            <div>
-                You are asking the owner about this property.
-            </div>
-            <span className="w-full border-t border-black"></span>
-            {data &&
-                <div className="flex flex-row gap-x-3">
-                    <div className="relative flex aspect-square w-16 h-16 items-center justify-center overflow-hidden rounded-lg">
-                        <Image
-                            src={img}
-                            alt="ไอบอสสสส"
-                            draggable={false}
-                            fill={true}
-                            style={{ objectFit: "cover" }}
-
-                        />
-                    </div>
-                    <div className="w-3/5 flex flex-col justify-center ">
-                        <div className="text-lg font-bold">{data?.property_name}</div>
-                        <div className="break-all truncate">{data?.address}</div>
-                    </div>
-                </div>}
-
+  return (
+    <div className="flex w-full flex-col gap-y-2 break-words rounded-xl bg-ci-light-gray px-4 py-3">
+      <div>You are asking the owner about this property.</div>
+      <span className="w-full border-t border-black"></span>
+      {data && (
+        <div className="flex flex-row gap-x-3">
+          <div className="relative flex aspect-square size-16 items-center justify-center overflow-hidden rounded-lg">
+            <Image
+              src={img}
+              alt="ไอบอสสสส"
+              draggable={false}
+              fill={true}
+              style={{ objectFit: "cover" }}
+            />
+          </div>
+          <div className="flex w-3/5 flex-col justify-center ">
+            <div className="text-lg font-bold">{data?.property_name}</div>
+            <div className="truncate break-all">{data?.address}</div>
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 }
